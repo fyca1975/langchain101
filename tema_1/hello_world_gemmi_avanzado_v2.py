a4b021602a4ae1ce177b46ba8f9c9dc051c5778e
Fredy Yecid Castro Agray <fredy.castro@gmail.com>
2026-02-11 14:35:22 -0500

    utilizando chain y StrOutputParser

--- tema_1/hello_world_gemmi_avanzado_v2.py
+++ tema_1/hello_world_gemmi_avanzado_v2.py
@@ -7,5 +7,10 @@ load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7)
 plantilla = PromptTemplate(input_variables=["nombre"], 
-                           template="Saluda al usuario con su nombre. \nNombre de usuario :{nombre}")
+                           template="Saluda al usuario con su nombre, dale una binevenida interesante " \
+                           "\nNombre de usuario :{nombre}")
 chain = plantilla | llm | StrOutputParser()
+
+#resultado = chain.run(nombre="Fredy")
+resultado = chain.invoke({"nombre": "Fredy"})
+print(resultado)
